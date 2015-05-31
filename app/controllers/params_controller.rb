9c8bf9087e859ed46ebc66b3d902618931263c9d
Jay Wengrow <jaywngrw@gmail.com>
2015-05-31 11:46:14 -0500

    demoed url segment parameters

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -4,6 +4,10 @@ class ParamsController < ApplicationController
     @second_message = params[:message2]
   end
 
+  def url_segments
+    @message = params[:wildcard]
+  end
+
   def capitalize
     @name = params[:name].upcase
     if @name.start_with?("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/get_params_example' => 'params#get_params_example'
   get '/name_capitalizer' => 'params#capitalize'
   get '/game' => 'params#number_game'
+
+
+  get '/url_segments_example/:wildcard/' => 'params#url_segments'
 end
