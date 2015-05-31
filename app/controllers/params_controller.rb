226c09dd3020e1bce6b82c1e3d4c44127c289e74
Jay Wengrow <jaywngrw@gmail.com>
2015-05-31 11:32:42 -0500

    added guess the number game and name capitalizer

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -3,4 +3,25 @@ class ParamsController < ApplicationController
     @message = params[:word]
     @second_message = params[:message2]
   end
+
+  def capitalize
+    @name = params[:name].upcase
+    if @name.start_with?("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+      @message = "Hey, that's not a real name!"
+    end
+  end
+
+  def number_game
+    @guess = params[:guess].to_i
+    winning_number = 36
+
+    if @guess > winning_number
+      @message = "Guess lower!"
+    elsif @guess < winning_number
+      @message = "Guess higher!"
+    else
+      @message = "Congrats! You've won!"
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'get_params_example' => 'params#get_params_example'
+  get '/get_params_example' => 'params#get_params_example'
+  get '/name_capitalizer' => 'params#capitalize'
+  get '/game' => 'params#number_game'
 end
